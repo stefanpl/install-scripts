ceba9bdbcfa0c944735553cbe575d815e8c9b507
Stefan Pl <sploesser@vrm.com>
2015-07-20 13:58:13 +0200

    remove username from zsh install script

--- install_zsh.sh
+++ install_zsh.sh
@@ -2,5 +2,4 @@
 git clone https://github.com/stefanpl/oh-my-zsh $HOME/.oh-my-zsh ;
 ln -s $HOME/.oh-my-zsh/.zshrc $HOME
 sudo apt-get install -y zsh
-sudo usermod -s /bin/zsh stefan
 touch $HOME/.bash_aliases_private
