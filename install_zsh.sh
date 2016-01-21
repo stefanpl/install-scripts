1c3cd62f309ee893e3330b52047e366ac8da69ea
spl <stefanpl@gmx.net>
2016-01-21 17:57:01 +0100

    zsh install script improved again

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,28 +1,30 @@
 #!bin/bash
 if [ -d ~/.oh-my-zsh ] && [ -h ~/.zshrc ]
 then
-	# oh-my-zsh seems to be installed. Check if zsh needs to be installed
-	zsh --version 2>/dev/null > /dev/null
-	if [ $? -eq 0 ]
-	then
-		echo "zsh already installed."
-	else
-		echo "installing z shell"
-		sudo apt-get install -y zsh
-	fi
-
-	# just run usermod command. There's no harm if zsh is already set
-	# first double-check if zsh is available at /bin/zsh
-	if [ -x /bin/zsh ]
-	then 
-		sudo usermod -s /bin/zsh $USER
-	else
-		echo "something seems odd. zsh should be installed but is not available at /bin/zsh"
-		exit 1
-	fi
-
+	echo "oh-my-zsh sees to be ready"
 else
 	# setup oh-my-zsh
 	git clone https://github.com/stefanpl/oh-my-zsh $HOME/.oh-my-zsh ;
 	ln -s ~/.oh-my-zsh/.zshrc ~
 fi
+
+# Check if zsh needs to be installed
+zsh --version 2>/dev/null > /dev/null
+if [ $? -eq 0 ]
+then
+	echo "zsh already installed."
+else
+	echo "installing z shell"
+	sudo apt-get install -y zsh
+fi
+
+# just run usermod command. There's no harm if zsh is already set
+# first double-check if zsh is available at /bin/zsh
+if [ -x /bin/zsh ]
+then 
+	sudo usermod -s /bin/zsh $USER
+else
+	echo "something seems odd. zsh should be installed but is not available at /bin/zsh"
+	exit 1
+fi
+
