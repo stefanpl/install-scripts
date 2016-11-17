df8c159dd4369f9d605a5a536f13df5d08fe4006
spl <stefanpl@gmx.net>
2016-11-17 22:25:50 +0100

    improved quicktile installation

--- install_quicktile.sh
+++ install_quicktile.sh
@@ -1,9 +1,34 @@
 #!/bin/bash
+
+# get dependencies
 sudo apt-get install -y python python-gtk2 python-xlib python-dbus python-wnck
-git clone https://github.com/ssokolow/quicktile.git /tmp/tmp_quicktile
-mkdir -p $HOME/scripts
-cp /tmp/tmp_quicktile/quicktile.py $HOME/scripts
-chmod +x $HOME/scripts/quicktile.py
-$HOME/scripts/quicktile.py
-cd /tmp/tmp_quicktile
-sudo /tmp/tmp_quicktile/setup.py install
+
+# get the script
+sudo curl -o /opt/quicktile.py https://raw.githubusercontent.com/ssokolow/quicktile/master/quicktile.py
+sudo chmod +x /opt/quicktile.py 
+sudo chown "$USER":"$USER" /opt/quicktile.py 
+
+# make it autostart
+autostart_dir="$HOME"/.config/autostart/
+autostart_file="$autostart_dir"quicktile
+mkdir -p $autostart_dir
+echo "[Desktop Entry]" >> autostart_file
+echo "Type=Application" >> autostart_file
+echo "Name=Quicktile" >> autostart_file
+echo "Exec=/opt/quicktile.py --daemonize" >> autostart_file
+
+# get the configuration file
+if [ ! -d "$HOME"/dotfiles ]; then
+	git clone https://github.com/stefanpl/dotfiles $HOME
+fi
+if [ -f "$HOME"/dotfiles/quicktile.cfg ]; then
+	ln -s "$HOME"/dotfiles/quicktile.cfg "$HOME"/.config/
+else
+	echo ERROR: found "$HOME"/dotfiles but no quicktile.cfg in it
+	exit 2
+fi
+
+# fiah it up right now
+/opt/quicktile.py --daemonize &
+
+
