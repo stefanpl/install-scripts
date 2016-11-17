afb2f2b36b1f9270d49f88eef52de446b6545981
spl <stefanpl@gmx.net>
2016-11-17 21:53:27 +0100

    only install synapse from ppa if necessary

--- install_synapse.sh
+++ install_synapse.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo apt-add-repository ppa:synapse-core/testing
-sudo apt-get update
+# check if it exists in package files - otherwise, use PPA
+if [[ ! $(sudo apt-cache search synapse | grep "^synapse ") ]]; then
+	echo "Synapse not found in repository. Adding PPA."
+	sudo apt-add-repository ppa:synapse-core/ppa
+	sudo apt-get update
+fi
 sudo apt-get install -y synapse
