49ec31b6fe09db9bd5fc3a2129ea7e4f395138fd
Stefan Ploesser <st.ploesser@googlemail.com>
2016-12-22 10:59:35 +0100

    dpkg -i without -y

--- install_chrome.sh
+++ install_chrome.sh
@@ -2,7 +2,7 @@
 if [[ $(uname -a | grep "x86_64") ]]; then
 	pushd /tmp/
 	curl -o chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo dpkg -y -i chrome.deb
+	sudo dpkg -i chrome.deb
 	popd > /dev/null
 else
 	echo "Chrome does not support 32bit systems any longer. chromium-browser package might be an alternative."
