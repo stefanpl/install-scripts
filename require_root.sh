9c6505993abd8d1d0dac3cae8df9fb03948be5e7
spl <stefanpl@gmx.net>
2017-10-06 10:56:03 +0200

    require_root script adjusts stefan and /home/stefan

--- require_root.sh
+++ require_root.sh
@@ -3,5 +3,9 @@ if [ "$EUID" -ne 0  ]
 then echo "Please run as root"
 	exit
 fi
-USER=$(logname || whoami)
-echo "Installing for user $USER"
+# We are interested in getting the user running the script
+if [ ! "$SUDO_USER" = '' ]; then
+	USER=$SUDO_USER
+fi
+# Reset $HOME with corrected user
+HOME=$( getent passwd "$user" | cut -d: -f6 )
