6a9080024ab0ac869f4f01a846b7a05d45ac86ac
Stefan Plösser <stefan@rarelime.de>
2018-06-02 10:18:37 +0200

    Full sublime install

--- install_sublime.sh
+++ install_sublime.sh
@@ -2,25 +2,20 @@
 source $( dirname $0  )/utils.sh
 # Requires the user to enter the sudo password
 sudo echo "something" > /dev/null || exitWithError "Sudo password required to run this script"
-#requireRoot
-#
-#wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-#apt-get install apt-transport-https
-#
-#sourceListFile=/etc/apt/sources.list.d/sublime-text.list
-#if [ ! -f ${sourceListFile} ]; then
-#	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee ${sourceListFile}
-#else
-#	logInfo "Sublime source list already existing at ${sourceListFile}."
-#fi
-#
-#sudo apt-get update
-#sudo apt-get install -y sublime-text
+
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+apt-get install apt-transport-https
+sourceListFile=/etc/apt/sources.list.d/sublime-text.list
+if [ ! -f ${sourceListFile} ]; then
+	echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee ${sourceListFile}
+else
+	logInfo "Sublime source list already existing at ${sourceListFile}."
+fi
+
+sudo apt-get update
+sudo apt-get install -y sublime-text
 
 # Install package control
-
-
-
 packageDirectory=${HOME}/.config/sublime-text-3/Installed\ Packages/ 
 mkdir -p "${packageDirectory}" 
 cd "${packageDirectory}"
