8e04e9745531be9366c25dac0f07f0cecd657b38
Stefan Plösser <stefan@rarelime.de>
2019-02-04 10:20:58 +0100

    install script for certbot

---
+++ install_certbot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install software-properties-common
+add-apt-repository universe
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install certbot python-certbot-nginx 
