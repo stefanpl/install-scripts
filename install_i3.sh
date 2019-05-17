61cf91ce192575898488f3eb6937b6c0b45d78d8
Stefan Plösser <stefan@rarelime.de>
2019-05-17 09:32:45 +0200

    install latest i3

---
+++ install_i3.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+source $( dirname $0  )/utils.sh
+requireRoot
+cd `mktemp -d`
+
+/usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2019.02.01_all.deb keyring.deb SHA256:176af52de1a976f103f9809920d80d02411ac5e763f695327de9fa6aff23f416
+
+dpkg -i ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+apt update
+apt install i3
+
+rm ./keyring.deb
